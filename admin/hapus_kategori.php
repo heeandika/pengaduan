c333f7b3cfb5b2b3f7deed75c5c4d7d9f443b1a6
Muhammad andika <heeandika@gmail.com>
2026-02-23 22:06:23 +0900

    make delete siswa and kategori

--- admin/hapus_kategori.php
+++ admin/hapus_kategori.php
@@ -1,3 +1,12 @@
 <?php 
+$id = (int)$_GET['id'];
 
+$stmt = $koneksi->prepare("DELETE FROM kategori WHERE id_kategori=?");
+$stmt->bind_param("i", $id);
+if ($stmt->execute()) {
+    header("Location: ?page=kategori");
+    exit();
+} else {
+    echo "ERROR:". $stmt->error;
+}
 ?>
--- admin/hapus_siswa.php
+++ admin/hapus_siswa.php
@@ -1,3 +1,12 @@
 <?php
+$id = (int)$_GET['id'];
 
+$stmt = $koneksi->prepare("DELETE FROM siswa WHERE id=?");
+$stmt->bind_param("i", $id);
+if ($stmt->execute()) {
+    header("Location: ?page=siswa");
+    exit();
+} else {
+    echo "ERROR:". $stmt->error;
+}
 ?>
