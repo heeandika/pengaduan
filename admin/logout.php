7da59d73ba009229e3843577813f7955d1243eb9
Muhammad andika <heeandika@gmail.com>
2026-02-23 21:10:50 +0900

    Make update and create in file siswa

--- admin/logout.php
+++ admin/logout.php
@@ -1,1 +1,5 @@
 <?php
+session_start();
+session_destroy();
+header("Locatrion: ../auth/login.php");
+exit();
--- admin/siswa.php
+++ admin/siswa.php
@@ -1,53 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>siswa</title>
 </head>
+
 <body>
     <h1>Siswa</h1>
     <?php
-    $id_siswa = "";
-    $NIS = "";
-    $kelas = "";
+    $id_siswa   = "";
+    $NIS        = "";
+    $kelas      = "";
 
     if (isset($_POST['submit'])) {
-        $NIS = $_POST['NIS'];
-        $kelas = $_POST['kelas'];
+
+        $id     = $_POST['id'] ?? "";
+        $NIS    = $_POST['NIS'];
+        $kelas  = $_POST['kelas'];
+
         if (empty($NIS) || empty($kelas)) {
-            $error = "harap isi semua data";
-            }if (!empty($id_siswa)) {
-            $id_siswa = $_POST['id_siswa'];
-            // $query = "UPDATE siswa SET NIS=$NIS, kelas=$kelas WHERE id_siswa=$id_siswa";
-            $x = $koneksi->query("UPDATE siswa SET NIS=$NIS, kelas=$kelas WHERE id_siswa=$id_siswa");
-            if ($x) {
-                header("Location: ?page=siswa");
-            }
+            $error = "Semua data harus diisi";
         } else {
-            $y = $koneksi->query("INSERT INTO siswa (NIS, kelas) VALUES($NIS, $kelas)");
-            if ($y) {
-                header("Location: ?page=siswa");
 
+            if (!empty($id)) {
+                $stmt = $koneksi->prepare("UPDATE siswa SET NIS=?, kelas=? WHERE id=?");
+                $stmt->bind_param("isi", $NIS, $kelas, $id);
+            } else {
+                $stmt = $koneksi->prepare("INSERT INTO siswa (NIS, kelas) VALUES (?, ?)");
+                $stmt->bind_param("is", $NIS, $kelas);
             }
-            
-        } 
-        
-        if (isset($_GET['id'])) {
-            $id =(int)$_GET['id'];
-
-
+        }
+        if ($stmt->execute()) {
+            header("Location: ?page=siswa");
+            exit();
+        } else {
+            $error = "Error: " . $stmt->error;
         }
     }
+
+    if (isset($_GET['id'])) {
+        $id   = (int)$_GET['id'] ?? "";
+        $stmt = $koneksi->prepare("SELECT * FROM siswa WHERE id=?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 0) {
+            exit("data kosong");
+        }
+        $data = $result->fetch_assoc();
+
+        $id_siswa   = $data['id'];
+        $NIS        = $data['NIS'];
+        $kelas      = $data['kelas'];
+    }
+
     ?>
     <form action="" method="post">
+
+        <input type="hidden" name="id" value="<?= $id_siswa ?>">
+
         <label for="NIS">NIS:</label>
-        <input type="number" name="NIS" required>
+        <input type="number" name="NIS" value="<?= $NIS ?>">
 
         <label for="kelas">Kelas:</label>
-        <input type="text" name="kelas" required>
+        <input type="text" name="kelas" value="<?= $kelas ?>">
 
-        <button type="submit" name="submit">submit</button>
+        <button type="submit" name="submit">
+
+            <?= !empty($id_siswa) ? "Ubah" : "Tambah" ?>
+            
+        </button>
+
     </form>
     <table border="1" cellpaddong="10" cellspacing="0">
         <tr>
@@ -65,10 +92,10 @@
                 echo "<td>" . $no++ . "</td>";
                 echo "<td>" . $row['NIS'] . "</td>";
                 echo "<td>" . $row['kelas'] . "</td>";
-                echo "<td>";
-                echo "<a href='?page=siswa&id=" . $row['id_siswa'] . "'>Edit </a>";
-                echo "<a href='?page=hapus_siswa&id=" . $row['NIS'] . "'>hapus </a>";
-                echo "</td>";
+                echo "<td>
+                <a href='?page=siswa&id="         . $row['id'] . "'>Edit </a>
+                <a href='?page=hapus_siswa&id="   . $row['id'] . "'onclick='return confirm(\"yakin?\")'>hapus</a>
+                </td>";
                 echo "</tr>";
             }
         }
@@ -76,4 +103,5 @@
 
     </table>
 </body>
+
 </html>
