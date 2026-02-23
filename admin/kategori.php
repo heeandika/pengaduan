9623ee716d7dab8f599fbd7f98401297dc1c25f5
Muhammad andika <heeandika@gmail.com>
2026-02-23 22:22:28 +0900

    make update and create for kategori

--- admin/kategori.php
+++ admin/kategori.php
@@ -1,16 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>siswa</title>
+    <title>kategori</title>
 </head>
+
 <body>
-    <h1>Kategori</h1>
-    <table>
+    <h1>kategori</h1>
+    <?php
+    $id_kategori    = "";
+    $ket_kategori   = "";
+
+    if (isset($_POST['submit'])) {
+
+        $id             = $_POST['id'] ?? "";;
+        $ket_kategori   = $_POST['ket_kategori'];
+
+        if (empty($ket_kategori)) {
+            $error = "Semua data harus diisi";
+        } else {
+
+            if (!empty($id)) {
+                $stmt = $koneksi->prepare("UPDATE kategori SET ket_kategori=? WHERE id_kategori=?");
+                $stmt->bind_param("si", $ket_kategori, $id);
+            } else {
+                $stmt = $koneksi->prepare("INSERT INTO kategori (ket_kategori) VALUES (?)");
+                $stmt->bind_param("s", $ket_kategori);
+            }
+        }
+        if (!$stmt->execute()) {
+            header("Location: ?page=kategori");
+            exit();
+        } else {
+            $error = "Error: " . $stmt->error;
+        }
+    }
+
+    if (isset($_GET['id'])) {
+        $id   = (int)$_GET['id'] ?? "";
+        $stmt = $koneksi->prepare("SELECT * FROM kategori WHERE id_kategori=?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 0) {
+            exit("data kosong");
+        }
+        $data = $result->fetch_assoc();
+
+        $id_kategori    = $data['id_kategori'];
+        $ket_kategori   = $data['ket_kategori'];
+    }
+
+    ?>
+    <form action="" method="post">
+
+        <input type="hidden" name="id" value="<?= $id_kategori ?>">
+
+        <label for="ket_kategori">Kategori:</label>
+        <input type="text" name="ket_kategori" value="<?= $ket_kategori ?>">
+
+        <button type="submit" name="submit">
+
+            <?= !empty($id_kategori) ? "Ubah" : "Tambah" ?>
+            
+        </button>
+
+    </form>
+    <table border="1" cellpaddong="10" cellspacing="0">
         <tr>
             <th>NO</th>
-            <th>Kategori</th>
+            <th>kategori</th>
             <th>Aksi</th>
         </tr>
         <?php
@@ -21,7 +84,10 @@
                 echo "<tr>";
                 echo "<td>" . $no++ . "</td>";
                 echo "<td>" . $row['ket_kategori'] . "</td>";
-                echo "<a href='?page=hapus_siswa&id=" . $row['id_kategori'] . "'>hapus</a>";
+                echo "<td>
+                <a href='?page=kategori&id="         . $row['id_kategori'] . "'>Edit </a>
+                <a href='?page=hapus_kategori&id="   . $row['id_kategori'] . "'onclick='return confirm(\"yakin?\")'>hapus</a>
+                </td>";
                 echo "</tr>";
             }
         }
@@ -29,4 +95,5 @@
 
     </table>
 </body>
+
 </html>
